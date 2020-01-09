26bc0b13e610c10f50c2a777729f9cd8df7bba45
DenisMarchiss <marchis.denis.marchis@gmail.com>
2020-01-09 17:05:30 +0200

    Done a Many to many relationshit between actors and movies

---
+++ MovieAPI/MovieAPI/Models/ActorsMovie.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieAPI.Models
+{
+    public partial class ActorsMovie
+    {
+        public int AcotorFkId { get; set; }
+        public int MovieFkId { get; set; }
+    }
+}
--- MovieAPI/MovieAPI/Models/Movies.cs
+++ MovieAPI/MovieAPI/Models/Movies.cs
@@ -5,16 +5,9 @@ namespace MovieAPI.Models
 {
     public partial class Movies
     {
-        public Movies()
-        {
-            Actors = new HashSet<Actors>();
-        }
-
         public int Id { get; set; }
         public string Name { get; set; }
         public string Crew { get; set; }
         public string Location { get; set; }
-
-        public virtual ICollection<Actors> Actors { get; set; }
     }
 }
